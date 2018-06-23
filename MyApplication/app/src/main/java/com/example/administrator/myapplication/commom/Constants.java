b154de4f3e11f5078ceb4b04e8ad0ee99020333d
Esonwang0923 <15209828293@163.com>
2018-06-23 14:56:51 +0800

    提交

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/commom/Constants.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/commom/Constants.java
@@ -2,7 +2,6 @@ package com.example.administrator.myapplication.commom;
 
 public class Constants {
 
-
     //public static String ip ="127.0.0.1";
     public static String ip ="140.143.230.126";
     public static String port ="8080";
@@ -12,7 +11,6 @@ public class Constants {
     public static String findAllByuserId ="http://"+ip+":"+port+"/article/findAllByuserId/";
     //删除文章
     public static String deleteUserArticle= "http://"+ip+":"+port+"/article/deleteUserArticle/";
-
     //设置已读
     public static String modifyRead= "http://"+ip+":"+port+"/article/modifyRead/";
     //登录校验
